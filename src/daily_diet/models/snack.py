cb42af9ef5325b258bce282525a58b4ae5b8ab03
Maria Andreza C Santos <mandreza008@gmail.com>
2025-02-05 05:25:03 -0300

    config: adding db first model

---
+++ src/daily_diet/models/snack.py
@@ -0,0 +1,20 @@
+
+from ...repository.database import db
+
+
+class Dieta(db.Model):
+    """ - Nome, - Descrição, - Data e Hora, - Está dentro ou não da dieta """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=True)
+    description = db.Column(db.String(200), nullable=True)
+    creat_at = db.Column(db.Date, nullable=True)
+    in_dieta = db.Column(db.Boolean, default=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "creat_at": self.creat_at,
+            "in_dieta": self.in_dieta,
+        }
