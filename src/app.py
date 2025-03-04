2823b557165616306c03cc8d42e509a10251bc6d
Maria Andreza C Santos <mandreza008@gmail.com>
2025-03-03 23:36:53 -0300

    feat: add function created meals

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, render_template, request, send_file
-from repository.database import db
-from db_models.snack import Dieta
-from datetime import datetime, timedelta
-from diet.dieta import Diet
+from datetime import datetime
+
+from flask import Flask, request
 from flask_socketio import SocketIO
 
+from db_models.refeicoes import Refeicoes
+from repository.database import db
 
 app = Flask(__name__)
 
@@ -18,12 +18,21 @@ socketio = SocketIO(app)
 @app.route('/created', methods=['POST'])
 def create_diet():
     data = request.get_json()
-    print(data)
-    
-    return jsonify({"message": "Dados inválidos."}), 400
+    nova_refeicao = Refeicoes(
+        name=data["name"],
+        description=data.get("description", ""),
+        date_time=datetime.strptime(data["date_time"], "%Y-%m-%d %H:%M:%S"),
+        in_diet=data["in_diet"],
+    )
+    db.session.add(nova_refeicao)
+    db.session.commit()
+    return {'message': 'Meals created successfully'}, 201
 
 
 
 
-    
-    
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
