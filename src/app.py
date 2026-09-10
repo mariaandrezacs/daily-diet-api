17c27bdd7da8b91d956c59b4c7a7d1dae4a5e5cf
Maria Andreza C Santos <mandreza008@gmail.com>
2026-09-10 00:36:09 -0300

    chore: atualiza app

--- src/app.py
+++ src/app.py
@@ -171,4 +171,9 @@ def delete_refeicao(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5008)))
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5008)),
+        allow_unsafe_werkzeug=True
+    )
